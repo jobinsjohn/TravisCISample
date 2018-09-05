6fe8ea94eb899f62d4601b9d5e1b35ab3ed15f4b
shtnkgm <nakagami.shota@gmail.com>
2018-09-06 00:49:46 +0900

    initial commit

--- TravisCISampleTests/TravisCISampleTests.swift
+++ TravisCISampleTests/TravisCISampleTests.swift
@@ -21,16 +21,7 @@ class TravisCISampleTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_hoge() {
+        XCTAssertTrue(true)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
